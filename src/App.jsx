55024f16f6cd8a7d1bfb1c89a267cb07fa7ab1d8
FacundoPalmaricciotti <palmaricciotti2024@gmail.com>
2024-11-21 00:55:18 -0300

    Corregido el nombre del componente Thankyou y actualizado la ruta

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Router>
             <NavBar />
             <Routes>
+                <Route path="/" element={<Home/>} />
                 <Route path="/home" element={<Home/>} />
                 <Route path="/cart" element={<Cart/>} />
                 <Route path="/product/:id" element={<Product/>} />
